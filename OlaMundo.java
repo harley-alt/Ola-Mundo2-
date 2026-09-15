5047557a632d7b246a59b84ced98e0c1c09dafd6
harley-alt <harleyqueiroz413@gmail.com>
2026-09-15 10:46:14 -0300

    Adicionei mais frases ao programa OlaMundo

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,5 +1,14 @@
 public class OlaMundo {
-    public static void main(String[] args) {
-        System.out.println("Olá,Mundo!");
-    }
+
+        public static void main(String[] args)}
+
+            System.out.println("Olá, Mundo!");
+            System.out.println("Meu primeiro programa em Java!");
+            System.out.println("Aprendendo a usar Git e Github!");
+            System.out.println("A prática leva a perfeição!");
+
+    
+    
+       
+        }
 }
